e8d507a9ee285d84c401fc58b5de4b20552f5f8b
Kiran Anand <liteninkiran@hotmail.com>
2021-02-01 01:31:35 +0000

    Initial commit | Forked

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home/{id}', 'HomeController@show');
+
+Route::get('admin', 'AdminController@index');
+
+
+// Route::get('/posts', 'PostsController@index');
+// Route::get('/posts/create', 'PostsController@create');
+// Route::get('/posts/{post}', 'PostsController@show');
+// Route::post('/posts', 'PostsController@store');
+// Route::get('/posts/{post}/edit', 'PostsController@edit');
+// Route::patch('/posts/{post}', 'PostsController@update');
+// Route::delete('/posts/{post}', 'PostsController@destroy');
+
+Route::resource('posts', 'PostsController');
+Route::resource('users', 'UsersController')->middleware('role:admin,manager');
+Route::resource('roles', 'RolesController')->middleware('can:isAdmin');
+
+Route::get('users', 'UsersController@index')->name('users.index');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
